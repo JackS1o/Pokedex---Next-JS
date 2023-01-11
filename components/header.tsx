41b49dadf4156e58daab0af10a7a760ee5c470dd
Jackson <jviana116@gmail.com>
2023-01-11 11:51:01 -0300

    feat(adicionando funcões para gerencianmento do input e button):

--- components/header.tsx
+++ components/header.tsx
@@ -1,25 +1,24 @@
 import styled from 'styled-components'
 
+const HeaderStyle = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 80px;
+  width: 100vw;
+  background-color: #dd4b4a;
+`;
+
+const Image = styled.img`
+  height: 35px;
+  padding: 0 30px;
+`;
+
 export default function Header() {
-  const Header = styled.header`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    height: 80px;
-    width: 100vw;
-    background-color: #dd4b4a;
-  `;
-
-  const Image = styled.img`
-    height: 35px;
-    padding: 0 30px;
-  `;
-
-
   return (
-    <Header>
+    <HeaderStyle>
       <Image src="https://cdn.riderize.com/miscellaneous/logo-pokedex.png" alt="logo" />
       <Image src="../settings.svg" alt="settings" />
-    </Header>
+    </HeaderStyle>
   )
 }
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,16 +1,87 @@
 import Header from "../components/header";
+import styled from "styled-components";
+import { ChangeEvent, SetStateAction, useState } from "react";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 80vh;
+`;
+
+const Image = styled.img`
+  height: 100px;
+  width: 100px;
+  margin-bottom: 30px;
+`;
+
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Input = styled.input`
+  height: 40px;
+  width: 300px;
+  margin-bottom: 10px;
+  font-size: 16px;
+`;
+
+const Button = styled.button`
+  height: 30px;
+  width: 300px;
+  background-color: #1555d1;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  &:hover {
+    background-color: #0d3b9a;
+    transition: 0.1s;
+    transform: scaleY(1.1);
+  }
+  font-weight: bold;
+`;
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setEmail(value);
+  };
+  
+  const login = () => {
+    const verifyEmail = /\S+@\S+\.\S+/;
+    const result = verifyEmail.test(email);
+    if (!result || email === "" || email.length < 5) {
+      alert("Digite um e-mail válido!");
+    }
+  }
+
   return (
     <>
       <Header />
-      <div>
-        <img src="../pokeball.svg" alt="pokeball" />
-        <div>
-          <input type="email" name="email" placeholder="email" />
-          <button type="submit">Acessar</button>
-        </div>
-      </div>
+      <Container>
+        <Image src="../pokeball.svg" alt="pokeball" />
+        <Div>
+          <Input
+            type="email"
+            value={email}
+            placeholder="Seu melhor e-mail"
+            onChange={(
+              e: React.ChangeEvent<HTMLInputElement>,
+          ): void => handleChange(e)}
+          />
+          <Button
+            type="button"
+            onClick={login}
+            >Acessar</Button>
+        </Div>
+      </Container>
     </>
   );
 }
