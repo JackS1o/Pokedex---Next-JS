edd27fab4fce761f8b87683311f8452af30e29f8
Jackson <jviana116@gmail.com>
2023-01-12 04:51:58 -0300

    feat: criando interface PokeDetailInterface e fixando tipo de elementos

--- interfaces/pokeInterface.ts
+++ interfaces/pokeInterface.ts
@@ -1,5 +1,15 @@
-export default interface PokeInterface {
+export interface PokeInterface {
   name: string;
   url: string;
   id: number;
+  results: [];
+  types: [];
+  sprites: {
+    front_default: string;
+  };
+}
+
+export interface PokeDetailInterface {
+  types: [{ type: { name: string }}];
+  name: string;
 }
--- pages/homePage.tsx
+++ pages/homePage.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 import { requestToApi, requestToApiWithParams } from "./api/pokeApi";
-import PokeInterface from "../interfaces/pokeInterface";
+import { PokeInterface, PokeDetailInterface } from "../interfaces/pokeInterface";
 
 export default function HomePage() {
-  const [data, setData] = useState("");
+  const [data, setData] = useState({} as PokeInterface);
   const [selectedPokemon, setSelectedPokemon] = useState("");
-  const [selectedPokemonData, setSelectedPokemonData] = useState("");
+  const [selectedPokemonData, setSelectedPokemonData] = useState({} as PokeInterface);
 
   useEffect(() => {
     requestToApi().then((res) => setData(res));
@@ -37,13 +37,13 @@ export default function HomePage() {
         <div>
           {results?.map((result: PokeInterface, index: number) => {
             return (
-              <>
-                <p key={index} onClick={() => setSelectedPokemon(result.name)}>
+              <div key={index}>
+                <p onClick={() => setSelectedPokemon(result.name)}>
                   {`#${index + 1}`} -{" "}
                   {result.name[0].toUpperCase() + result.name.slice(1)}
                 </p>
                 <img src={result.url} alt="" />
-              </>
+              </div>
             );
           })}
         </div>
@@ -52,6 +52,15 @@ export default function HomePage() {
         <h1>{`#${selectedPokemonData.id}`}</h1>
         <h1>{selectedPokemonData.name}</h1>
         <img src={selectedPokemonData.sprites?.front_default} alt="pokemon" />
+      </div>
+      <div>
+        <img src={selectedPokemonData.sprites?.front_default} alt="pokemon" />
+        <div>
+          <p>Type</p>
+          {selectedPokemonData.types?.map((type: PokeDetailInterface, index: number) => (
+            <p key={index}>{type.name}</p>
+          ))}
+        </div>
       </div>
     </div>
   );
